248decaaa67b738241ad17bd20f7b0773ab982cb
Gegy <gegy1000@gmail.com>
2021-01-06 08:52:44 +0200

    no longer remove all withers

--- src/main/java/xyz/nucleoid/extras/NucleoidExtras.java
+++ src/main/java/xyz/nucleoid/extras/NucleoidExtras.java
@@ -2,10 +2,7 @@ package xyz.nucleoid.extras;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.world.ServerWorld;
 
 public final class NucleoidExtras implements ModInitializer {
     @Override
@@ -17,12 +14,6 @@ public final class NucleoidExtras implements ModInitializer {
         int ticks = server.getTicks();
         if (ticks % 20 == 0) {
             NucleoidSidebar.get(server).update();
-
-            for (ServerWorld world : server.getWorlds()) {
-                for (Entity entity : world.getEntitiesByType(EntityType.WITHER, entity -> true)) {
-                    entity.remove();
-                }
-            }
         }
     }
 }
