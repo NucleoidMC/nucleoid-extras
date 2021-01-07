7fbc804e6c739a90e87011cacae746348d009cd5
Gegy <gegy1000@gmail.com>
2021-01-07 11:28:02 +0200

    support receiving name color from discord through backend

--- src/main/java/xyz/nucleoid/extras/integrations/ChatRelayIntegration.java
+++ src/main/java/xyz/nucleoid/extras/integrations/ChatRelayIntegration.java
@@ -7,9 +7,12 @@ import net.minecraft.network.MessageType;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Util;
+import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.extras.event.PlayerSendChatEvent;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -33,7 +36,13 @@ public final class ChatRelayIntegration {
             integrations.bindReceiver("chat", body -> {
                 String sender = body.get("sender").getAsString();
                 String content = body.get("content").getAsString();
-                integration.messageQueue.add(new ChatMessage(sender, content.split("\n")));
+
+                TextColor nameColor = null;
+                if (body.has("name_color")) {
+                    nameColor = TextColor.fromRgb(body.get("name_color").getAsInt());
+                }
+
+                integration.messageQueue.add(new ChatMessage(sender, content.split("\n"), nameColor));
             });
 
             ServerTickEvents.END_SERVER_TICK.register(integration::tick);
@@ -65,8 +74,13 @@ public final class ChatRelayIntegration {
 
     private void broadcastMessage(MinecraftServer server, ChatMessage message) {
         for (String line : message.lines) {
-            Text text = new LiteralText("<@" + message.sender + "> ").formatted(Formatting.AQUA)
-                    .append(new LiteralText(line).formatted(Formatting.GRAY));
+            MutableText sender = new LiteralText(message.sender);
+            if (message.nameColor != null) {
+                sender = sender.setStyle(sender.getStyle().withColor(message.nameColor));
+            }
+
+            Text text = new LiteralText("<@").append(sender).append("> ").formatted(Formatting.GRAY)
+                    .append(new LiteralText(line).formatted(Formatting.RESET));
 
             server.getPlayerManager().broadcastChatMessage(text, MessageType.CHAT, Util.NIL_UUID);
         }
@@ -75,10 +89,12 @@ public final class ChatRelayIntegration {
     static class ChatMessage {
         final String sender;
         final String[] lines;
+        final TextColor nameColor;
 
-        ChatMessage(String sender, String[] lines) {
+        ChatMessage(String sender, String[] lines, @Nullable TextColor nameColor) {
             this.sender = sender;
             this.lines = lines;
+            this.nameColor = nameColor;
         }
     }
 }
