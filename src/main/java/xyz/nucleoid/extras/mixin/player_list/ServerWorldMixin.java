0e48c6987eec456ef7fff59927ef550152f4863c
Patbox <39821509+patbox@users.noreply.github.com>
2021-01-21 18:42:02 +0100

    Update player on player list while adding/removing teams
    Signed-off-by: Patbox <39821509+Patbox@users.noreply.github.com>

--- src/main/java/xyz/nucleoid/extras/mixin/player_list/ServerWorldMixin.java
+++ src/main/java/xyz/nucleoid/extras/mixin/player_list/ServerWorldMixin.java
@@ -1,6 +1,5 @@
 package xyz.nucleoid.extras.mixin.player_list;
 
-import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -20,25 +19,6 @@ public class ServerWorldMixin {
 
     @Inject(method = "addPlayer", at = @At("TAIL"))
     private void onPlayerJoinWorld(ServerPlayerEntity joinedPlayer, CallbackInfo ci) {
-        PlayerListS2CPacket normalPacket = PlayerListHelper.getUpdateDisplayNamePacket(joinedPlayer, false);
-        PlayerListS2CPacket grayPacket = PlayerListHelper.getUpdateDisplayNamePacket(joinedPlayer, true);
-
-        PlayerListS2CPacket updateJoined = new PlayerListS2CPacket();
-        ((PlayerListS2CPacketAccessor) updateJoined).nucleoid$setAction(PlayerListS2CPacket.Action.UPDATE_DISPLAY_NAME);
-
-        for (ServerPlayerEntity player : this.server.getPlayerManager().getPlayerList()) {
-            boolean gray = PlayerListHelper.shouldGray(player, joinedPlayer);
-
-            player.networkHandler.sendPacket(gray ? grayPacket : normalPacket);
-            PlayerListS2CPacket.Entry entry = updateJoined.new Entry(
-                    player.getGameProfile(),
-                    0,
-                    player.interactionManager.getGameMode(),
-                    PlayerListHelper.getDisplayName(player, gray)
-            );
-            ((PlayerListS2CPacketAccessor) updateJoined).nucleoid$getEntries().add(entry);
-        }
-
-        joinedPlayer.networkHandler.sendPacket(updateJoined);
+        PlayerListHelper.updatePlayer(joinedPlayer);
     }
 }
