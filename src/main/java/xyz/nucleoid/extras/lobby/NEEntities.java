9b1c7b0a04990734b5874debadf49006d228e3dc
Patbox <39821509+Patbox@users.noreply.github.com>
2021-08-29 09:30:03 +0200

    No-op some methods in QuickArmorStandEntity, make it invulnerable to players
    Signed-off-by: Patbox <39821509+Patbox@users.noreply.github.com>

--- src/main/java/xyz/nucleoid/extras/lobby/NEEntities.java
+++ src/main/java/xyz/nucleoid/extras/lobby/NEEntities.java
@@ -10,7 +10,7 @@ import xyz.nucleoid.extras.NucleoidExtras;
 import xyz.nucleoid.extras.lobby.entity.QuickArmorStandEntity;
 
 public class NEEntities {
-    public static final EntityType<QuickArmorStandEntity> QUICK_ARMOR_STAND_ENTITY_ENTITY_TYPE =
+    public static final EntityType<QuickArmorStandEntity> QUICK_ARMOR_STAND_ENTITY_TYPE =
             FabricEntityTypeBuilder.<QuickArmorStandEntity>create(SpawnGroup.MISC, QuickArmorStandEntity::new)
                     .dimensions(EntityDimensions.fixed(0.5f, 1.975f))
                     .trackRangeChunks(2)
@@ -19,8 +19,8 @@ public class NEEntities {
 
 
     public static void register() {
-        register("quick_armor_stand", QUICK_ARMOR_STAND_ENTITY_ENTITY_TYPE);
-        FabricDefaultAttributeRegistry.register(QUICK_ARMOR_STAND_ENTITY_ENTITY_TYPE, QuickArmorStandEntity.createLivingAttributes());
+        register("quick_armor_stand", QUICK_ARMOR_STAND_ENTITY_TYPE);
+        FabricDefaultAttributeRegistry.register(QUICK_ARMOR_STAND_ENTITY_TYPE, QuickArmorStandEntity.createLivingAttributes());
     }
 
     private static <T extends EntityType<?>> T register(String id, T item) {
--- src/main/java/xyz/nucleoid/extras/lobby/entity/QuickArmorStandEntity.java
+++ src/main/java/xyz/nucleoid/extras/lobby/entity/QuickArmorStandEntity.java
@@ -2,7 +2,9 @@ package xyz.nucleoid.extras.lobby.entity;
 
 import eu.pb4.polymer.entity.VirtualEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 import xyz.nucleoid.extras.lobby.NEEntities;
 
@@ -12,11 +14,30 @@ public class QuickArmorStandEntity extends ArmorStandEntity implements VirtualEn
     }
 
     public QuickArmorStandEntity(World world) {
-        super(NEEntities.QUICK_ARMOR_STAND_ENTITY_ENTITY_TYPE, world);
+        super(NEEntities.QUICK_ARMOR_STAND_ENTITY_TYPE, world);
     }
 
     @Override
     public EntityType<?> getVirtualEntityType() {
         return EntityType.ARMOR_STAND;
+    }
+
+    @Override
+    public boolean isInvulnerableTo(DamageSource damageSource) {
+        var attacker = damageSource.getAttacker();
+        if (attacker instanceof ServerPlayerEntity player && player.interactionManager.isSurvivalLike()) {
+            return true;
+        }
+        return super.isInvulnerableTo(damageSource);
+    }
+
+    @Override
+    protected void tickCramming() {
+
+    }
+
+    @Override
+    public void tickMovement() {
+
     }
 }
