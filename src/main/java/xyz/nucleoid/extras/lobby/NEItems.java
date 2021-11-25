ee314cec966493cf65aa8711a4d3710a43d05af0
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-11-25 11:49:44 -0500

    Add tuff taters

--- src/main/java/xyz/nucleoid/extras/lobby/NEItems.java
+++ src/main/java/xyz/nucleoid/extras/lobby/NEItems.java
@@ -41,6 +41,7 @@ public class NEItems {
     public static final Item AZALEA_TATER = createHead(NEBlocks.AZALEA_TATER);
     public static final Item STONE_TATER = createHead(NEBlocks.STONE_TATER);
     public static final Item CALCITE_TATER = createHead(NEBlocks.CALCITE_TATER);
+    public static final Item TUFF_TATER = createHead(NEBlocks.TUFF_TATER);
     public static final Item FLAME_TATER = createHead(NEBlocks.FLAME_TATER);
 
     public static final Item TATEROID = createHead(NEBlocks.TATEROID);
@@ -85,6 +86,7 @@ public class NEItems {
         register("azalea_tater", AZALEA_TATER);
         register("stone_tater", STONE_TATER);
         register("calcite_tater", CALCITE_TATER);
+        register("tuff_tater", TUFF_TATER);
         register("flame_tater", FLAME_TATER);
 
         register("tateroid", TATEROID);
