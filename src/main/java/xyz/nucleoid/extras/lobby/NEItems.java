4c5052d99af357f9d82add7e31c54bb7f3a62c8e
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-12-03 12:16:54 -0500

    Add dripstone taters

--- src/main/java/xyz/nucleoid/extras/lobby/NEItems.java
+++ src/main/java/xyz/nucleoid/extras/lobby/NEItems.java
@@ -42,6 +42,7 @@ public class NEItems {
     public static final Item STONE_TATER = createHead(NEBlocks.STONE_TATER);
     public static final Item CALCITE_TATER = createHead(NEBlocks.CALCITE_TATER);
     public static final Item TUFF_TATER = createHead(NEBlocks.TUFF_TATER);
+    public static final Item DRIPSTONE_TATER = createHead(NEBlocks.DRIPSTONE_TATER);
     public static final Item FLAME_TATER = createHead(NEBlocks.FLAME_TATER);
     public static final Item TATER_OF_UNDYING = createHead(NEBlocks.TATER_OF_UNDYING);
 
@@ -88,6 +89,7 @@ public class NEItems {
         register("stone_tater", STONE_TATER);
         register("calcite_tater", CALCITE_TATER);
         register("tuff_tater", TUFF_TATER);
+        register("dripstone_tater", DRIPSTONE_TATER);
         register("flame_tater", FLAME_TATER);
         register("tater_of_undying", TATER_OF_UNDYING);
 
