b0c439b5b46769f5096bffd5cb103eb4d231b85e
Shnupbups <shnupbups@gmail.com>
2022-02-01 05:49:07 +1100

    i need professional help
    Added 30 more taters
    Azalea Tater, Bell Tater, Cold Frog Tater, Conduit Tater, Elder Guardian Tater, End Stone Brick Tater, Flower Pot Tater, Guardian Tater, Illager Tater, Illusioner Tater, Jukebox Tater, Lantern Tater, Piglin Tater, Pink Wither Tater, Piston Tater, Purpur Tater, Soul Lantern Tater, Soul Soil Tater, Sticky Piston Tater, Temperate Frog Tater, Underwater TNTater, Vex Tater, Villager Tater, Vinditater, Wandering Trader Tater, Warm Frog Tater, Wax Tater, Witch Tater, Zombie Villager Tater, Zombified Piglin Tater
    
    Added 11 more advancements
    Modified existing advancements to account for new additions
    Renamed existing Azalea Tater to Flowering Azalea Tater
    Advancement criteria will now throw an error if a non-existent tater is specified in them
    Added 'wear tater' advancement criteria, can specify the day of the week it's bein g worn. You know why.

--- src/main/java/xyz/nucleoid/extras/lobby/NECriteria.java
+++ src/main/java/xyz/nucleoid/extras/lobby/NECriteria.java
@@ -1,13 +1,16 @@
 package xyz.nucleoid.extras.lobby;
 
 import xyz.nucleoid.extras.lobby.criterion.TaterCollectedCriterion;
+import xyz.nucleoid.extras.lobby.criterion.WearTaterCriterion;
 
 import net.fabricmc.fabric.api.object.builder.v1.advancement.CriterionRegistry;
 
 public class NECriteria {
 	public static final TaterCollectedCriterion TATER_COLLECTED = new TaterCollectedCriterion();
+	public static final WearTaterCriterion WEAR_TATER = new WearTaterCriterion();
 
 	public static void register() {
 		CriterionRegistry.register(TATER_COLLECTED);
+		CriterionRegistry.register(WEAR_TATER);
 	}
 }
