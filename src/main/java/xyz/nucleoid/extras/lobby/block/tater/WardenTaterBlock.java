dadd5d570fa3aac11f2ceba8a5cc4f8803d49ddf
haykam821 <24855774+haykam821@users.noreply.github.com>
2023-09-07 17:48:07 -0400

    Fix warden tater particles being positioned incorrectly

--- src/main/java/xyz/nucleoid/extras/lobby/block/tater/WardenTaterBlock.java
+++ src/main/java/xyz/nucleoid/extras/lobby/block/tater/WardenTaterBlock.java
@@ -15,8 +15,6 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.event.BlockPositionSource;
-import net.minecraft.world.event.EntityPositionSource;
-import net.minecraft.world.event.PositionSource;
 
 public class WardenTaterBlock extends CubicPotatoBlock {
     private static final int BOX_SIZE = 16;
@@ -28,13 +26,19 @@ public class WardenTaterBlock extends CubicPotatoBlock {
 
     @Override
     public ParticleEffect getBlockParticleEffect(BlockState state, ServerWorld world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        return getTaterVibrationParticleEffect(pos, new BlockPositionSource(pos), world);
+        return getTaterVibrationParticleEffect(pos, world);
+    }
+
+    public void spawnBlockParticles(ServerWorld world, BlockPos pos, ParticleEffect particleEffect) {
+        if (particleEffect != null && world.getRandom().nextInt(getBlockParticleChance()) == 0) {
+            world.spawnParticles(particleEffect, pos.getX() + 0.5, pos.getY() + 0.25, pos.getZ() + 0.5, 1, 0, 0, 0, 0);
+        }
     }
 
     @Override
     public ParticleEffect getPlayerParticleEffect(ServerPlayerEntity player) {
-        BlockPos pos = BlockPos.ofFloored(player.getX(), player.getEyeY(), player.getZ());
-        return getTaterVibrationParticleEffect(pos, new EntityPositionSource(player, 0), player.getServerWorld());
+        BlockPos pos = BlockPos.ofFloored(player.getX(), player.getEyeY() - 0.2, player.getZ());
+        return getTaterVibrationParticleEffect(pos, player.getServerWorld());
     }
 
     @Override
@@ -42,7 +46,19 @@ public class WardenTaterBlock extends CubicPotatoBlock {
         return ARRIVAL_TICKS;
     }
 
-    private static ParticleEffect getTaterVibrationParticleEffect(BlockPos pos, PositionSource source, ServerWorld world) {
+    @Override
+    public void spawnPlayerParticles(ServerPlayerEntity player) {;
+        double x = player.getX();
+        double y = player.getEyeY() - 0.2;
+        double z = player.getZ();
+
+        ParticleEffect particleEffect = this.getPlayerParticleEffect(player);
+        if (particleEffect != null) {
+            player.getServerWorld().spawnParticles(particleEffect, x, y, z, 1, 0, 0, 0, 0);
+        }
+    }
+
+    private static ParticleEffect getTaterVibrationParticleEffect(BlockPos pos, ServerWorld world) {
         LongList taters = new LongArrayList();
 
         int range = (int) (BOX_SIZE / 2d);
