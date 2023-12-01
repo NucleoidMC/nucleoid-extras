a791e73ef4571d078d009eb54cab122c3b9d3b4e
Ashhhleyyy <ash@ashhhleyyy.dev>
2023-12-01 03:23:01 +0000

    feat: add nucleoid wrapped message

--- src/main/java/xyz/nucleoid/extras/NucleoidExtras.java
+++ src/main/java/xyz/nucleoid/extras/NucleoidExtras.java
@@ -3,7 +3,13 @@ package xyz.nucleoid.extras;
 import eu.pb4.playerdata.api.PlayerDataApi;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,6 +27,8 @@ import xyz.nucleoid.extras.lobby.contributor.ContributorData;
 import xyz.nucleoid.extras.placeholder.ExtraPlaceholders;
 import xyz.nucleoid.extras.scheduled_stop.ScheduledStop;
 import xyz.nucleoid.extras.sidebar.NucleoidSidebar;
+
+import java.util.Calendar;
 
 public final class NucleoidExtras implements ModInitializer {
     public static final String ID = "nucleoid_extras";
@@ -50,7 +58,22 @@ public final class NucleoidExtras implements ModInitializer {
         PlayerDataApi.register(PlayerLobbyState.STORAGE);
 
         ServerTickEvents.END_SERVER_TICK.register(NucleoidExtras::onServerTick);
+        ServerPlayConnectionEvents.JOIN.register(NucleoidExtras::onPlayerJoin);
         ServerChangePortalBackend.register();
+    }
+
+    private static void onPlayerJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
+        Calendar calendar = Calendar.getInstance();
+        if (calendar.get(Calendar.YEAR) == 2023 && calendar.get(Calendar.MONTH) == Calendar.DECEMBER) {
+            handler.getPlayer().sendMessage(
+                Text.translatable("text.nucleoid_extras.wrapped.join", handler.getPlayer().getUuidAsString())
+                    .formatted(Formatting.GREEN)
+                    .styled(style -> style.withClickEvent(new ClickEvent(
+                        ClickEvent.Action.OPEN_URL,
+                        "https://stats.nucleoid.xyz/players/" + handler.getPlayer().getUuidAsString() + "/wrapped"
+                    )))
+            );
+        }
     }
 
     private static void onServerTick(MinecraftServer server) {
