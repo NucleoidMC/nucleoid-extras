4a9084af23c5b7cd3fc02e27d7f30b5cde55f6cf
haykam821 <24855774+haykam821@users.noreply.github.com>
2024-06-08 03:01:49 -0400

    Use data generation for item tags

--- src/datagen/java/xyz/nucleoid/extras/data/NEDatagen.java
+++ src/datagen/java/xyz/nucleoid/extras/data/NEDatagen.java
@@ -3,12 +3,14 @@ package xyz.nucleoid.extras.data;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import xyz.nucleoid.extras.data.provider.NEBlockTagProvider;
+import xyz.nucleoid.extras.data.provider.NEItemTagProvider;
 
 public class NEDatagen implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator dataGenerator) {
-        FabricDataGenerator.Pack pack = dataGenerator.createPack();
+        var pack = dataGenerator.createPack();
 
-        pack.addProvider(NEBlockTagProvider::new);
+        var blockTags = pack.addProvider(NEBlockTagProvider::new);
+        pack.addProvider((dataOutput, registries) -> new NEItemTagProvider(dataOutput, registries, blockTags));
     }
 }
