87bf88ceffd8a9ab9565b087b9e0226378615735
Patbox <39821509+Patbox@users.noreply.github.com>
2024-12-14 22:00:39 +0100

    Make invulnerable entities require player attack

--- src/main/java/xyz/nucleoid/extras/mixin/patches/EntityMixin.java
+++ src/main/java/xyz/nucleoid/extras/mixin/patches/EntityMixin.java
@@ -2,6 +2,7 @@ package xyz.nucleoid.extras.mixin.patches;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.registry.tag.DamageTypeTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,7 +20,7 @@ public class EntityMixin {
      */
     @Inject(method = "isAlwaysInvulnerableTo", at = @At("HEAD"), cancellable = true)
     private void extras$makeExceptionForExplosions(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
-        if (this.invulnerable && damageSource.isIn(DamageTypeTags.IS_EXPLOSION)) {
+        if (this.invulnerable && !damageSource.isOf(DamageTypes.PLAYER_ATTACK)) {
             cir.setReturnValue(true);
         }
     }
