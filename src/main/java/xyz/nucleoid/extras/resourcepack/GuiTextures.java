202f8b22a2a45941d653537b06c0e454e3b27628
Patbox <39821509+Patbox@users.noreply.github.com>
2025-07-09 21:29:43 +0200

    Bridge sgui

--- src/main/java/xyz/nucleoid/extras/resourcepack/GuiTextures.java
+++ src/main/java/xyz/nucleoid/extras/resourcepack/GuiTextures.java
@@ -1,12 +1,15 @@
 package xyz.nucleoid.extras.resourcepack;
 
+import eu.pb4.polymer.resourcepack.extras.api.ResourcePackExtras;
 import eu.pb4.sgui.api.elements.GuiElement;
 import eu.pb4.sgui.api.elements.GuiElementBuilder;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import static xyz.nucleoid.extras.NucleoidExtras.identifier;
 import static xyz.nucleoid.extras.resourcepack.UiResourceCreator.*;
 
 public class GuiTextures {
@@ -19,7 +22,7 @@ public class GuiTextures {
     public static final char SPACE_1 = UiResourceCreator.space(1);
 
     public static void register() {
-
+        ResourcePackExtras.forDefault().addBridgedModelsFolder(identifier("sgui"));
     }
 
 }
